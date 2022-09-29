d76cae3ababb9e91dd4d9a84dadc6b4c20ed6169
AdamBohlson <39510283+AdamBohlson@users.noreply.github.com>
2022-09-29 10:46:46 -0700

    Functioning image upload, manip, and download
    the hard part comes next, figuring out how I want to change each pixel...

--- MapFun/src/ImageManip.java
+++ MapFun/src/ImageManip.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
@@ -19,29 +20,34 @@ public class ImageManip {
 		}
 		catch (IOException e)
 		{
-			System.out.println("Error: "+e);
+			System.out.println("Error: " + e);
 		}
 		
-		int rgb = image.getRGB(0,0);
-		int alpha = (rgb >> 24) & 0xFF;
-		int red =   (rgb >> 16) & 0xFF;
-		int green = (rgb >>  8) & 0xFF;
-		int blue =  (rgb      ) & 0xFF;
-		System.out.println("A: " + alpha + " R: " + red + " G: " + green + " B: " + blue);
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
-				
+				int[] argb = getARGB(image, x, y);
+				//manipulation done here
+				setARGB(image, x, y, argb);
 			}
 		}
-		/*
+		
 		try {
 			f = new File("O:\\Vanatas\\Vanatas_Topo_BWB_edited.png");
 			ImageIO.write(image, "png", f);
 			System.out.println("Writing complete.");
 	    }
 		catch(IOException e) {
-			System.out.println("Error: "+e);
+			System.out.println("Error: " + e);
 	    }
-	    */
+	}
+	
+	public static int[] getARGB(BufferedImage image, int x, int y) {
+		Color pixle = new Color(image.getRGB(x, y));
+		int[] argb = {pixle.getAlpha(), pixle.getRed(), pixle.getGreen(), pixle.getBlue()};
+		return argb;
+	}
+	
+	public static void setARGB(BufferedImage image, int x, int y, int[] rgb) {
+		image.setRGB(x, y, (new Color(rgb[1], rgb[2], rgb[3], rgb[0]).getRGB()));
 	}
 }
